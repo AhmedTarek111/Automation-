d2488e4ebb47c66fc350fd137469a82359a6b254
Ahmed Tarek Alsaudi <ahmedtarekalsaudi@gmail.com>
2023-05-15 22:46:19 -0400

    remaining days before birth day
    calculate the remaining day for the birthdate

---
+++ Birthday_Calculator.py
@@ -0,0 +1,14 @@
+from datetime import date
+def  Birthday_Calculator():
+    
+    user_birthdate=date.fromisoformat(input("your birthday date  : "))
+    now=date.today()
+    if (user_birthdate.month, user_birthdate.day) < (now.month, now.day):
+        next_birthday = date(now.year + 1, user_birthdate.month, user_birthdate.day)
+    else:
+        next_birthday = date(now.year, user_birthdate.month, user_birthdate.day)
+    remaining = next_birthday - now
+    remaining_days = remaining.days
+
+    return f"{remaining_days} days Left for your birthdate"
+print(Birthday_Calculator())
